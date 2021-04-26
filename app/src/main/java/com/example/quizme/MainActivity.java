af0d5510b9ef1f831be5ef1acf4e418046eff9b5
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2021-04-26 14:44:33 +0200

    wip

--- app/src/main/java/com/example/quizme/MainActivity.java
+++ app/src/main/java/com/example/quizme/MainActivity.java
@@ -1,49 +1,41 @@
 package com.example.quizme;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    // GUI control
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        // Initializing list view with the custom adapter
+        //ArrayList<Player> playerList = new ArrayList<>();
+        buttonStart = findViewById(R.id.btnStart);
+
+        // Set up list item onclick listener
+        setUpListItemClickListener();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void setUpListItemClickListener() {
+
+        buttonStart.setOnClickListener((View v) -> {
+
+                //Toast.makeText(getApplicationContext(), "Player " + position + " clicked", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, ShowAllSetsActivity.class);
+                intent.putExtra("PLAYER_NAME", "playerName");
+
+                startActivity(intent );
+        });
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 }
