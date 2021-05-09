58a20933a9958c620713a3a1b5fef9cc9ae442a6
vetwrf <83170288+vetwrf@users.noreply.github.com>
2021-05-09 18:16:24 +0200

    Create PlayQuiz.java

---
+++ app/src/main/java/com/example/quizme/PlayQuiz.java
@@ -0,0 +1,26 @@
+package com.example.quizme;
+
+
+public class PlayQuiz {
+    private int rights;
+    private int round;
+    PlayQuiz(Sting set){
+        set = db.selsctSet(set);
+    }
+    public void roundPlay(){
+        questions[round*4]
+    }
+    public String getQuestion(){
+        return questions[round*4];
+    }
+    public String[3] getChoises(){
+        return questions[round*4];
+    }
+
+
+
+    public int getRights(){
+        return rights;
+    }
+
+}
