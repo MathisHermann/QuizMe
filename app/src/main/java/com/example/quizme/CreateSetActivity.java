f39aa40fec6678605fe13bd31622eead4243251d
keianig <81552948+keianig@users.noreply.github.com>
2021-05-16 17:09:26 +0200

    neue Activities

---
+++ app/src/main/java/com/example/quizme/CreateSetActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateSetActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_set);
+    }
+    private static void createSet(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/quizme/EditSetActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditSetActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_set);
+    }
+
+
+}
